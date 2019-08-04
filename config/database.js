f87a7e4e0f042bc7162a5ca2b3bd065a49738da6
Utsav <ukaran11@gmail.com>
2019-08-04 23:07:17 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://utsav:utsav123@ds259377.mlab.com:59377/mean-auth-app',
+    database: 'mongodb://utsav:gokussj3@ds259377.mlab.com:59377/mean-auth-app',
     secret: 'yoursecret'
 }
